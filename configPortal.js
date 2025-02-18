c282723b844a86a3751b8fcf7e1a2ab256797136
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2025-02-17 20:56:34 -0500

    config portal - update config in redis  based on form

--- configPortal.js
+++ configPortal.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import redisClient from './config/redisClient.js'
 
 dotenv.config()
 
@@ -17,15 +18,29 @@ const defaultConfig = {
 }
 
 app.get('/', async(req, res)=>{
-    res.render('dashboard', {
-        config:{
-           bucketSize: defaultConfig.bucketSize,
-           refillRate: defaultConfig.refillRate, 
-        }
+    const data = await redisClient.hGetAll('rateLimiterConfig');
+    console.log(data);
+    if (Object.keys(data).length > 0){
+        return res.render('dashboard', {config: {
+            bucketSize: parseInt(data.bucketSize),
+            refillRate: parseInt(data.refillRate)
+        }})
+    }
+    return res.render('dashboard', {
+        config:defaultConfig
     })
 })
 
-
+app.post('/config', async(req, res)=>{
+    const {bucketSize, refillRate} = req.body;
+    console.log(bucketSize, refillRate)
+    if (bucketSize && refillRate){
+        await redisClient.hSet('rateLimiterConfig', 'bucketSize', bucketSize);
+        await redisClient.hSet('rateLimiterConfig', 'refillRate', refillRate);
+        return res.status(200).redirect('/');
+    }
+    res.status(400).send('Invalid Data')
+})
 app.listen(port, ()=>{
     console.log('Config portal running on 3001');
 })
